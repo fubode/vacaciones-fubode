5ddb98e5fd52480eaadde7b4e2144cc618580401
Juan pablo <201400076@est.umss.edu>
2022-12-19 05:19:03 -0400

    cambios realisados para entrega de pre experimento

--- src/java/Modelo/DriveManager.java
+++ src/java/Modelo/DriveManager.java
@@ -10,12 +10,20 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
  *
  * @author GEEK STORE
  */
-public class DriveManager extends DriverManagerDataSource{
-    public DriveManager(){
-        this.setDriverClassName("org.postgresql.Driver");
-        this.setUrl("jdbc:postgresql://ec2-3-217-113-25.compute-1.amazonaws.com/dfq426bctojb4q");
-        this.setUsername("kkpyqhrsqaehba");
-        this.setPassword("e5698668b1d3334b6a6f5a8af8db54740bb49b5e69bda7bb4175191253441910");
+public class DriveManager extends DriverManagerDataSource {
+
+    private final String DRIVEMANAGER = "org.postgresql.Drive";
+    private final String URL = "jdbc:postgresql://localhost:5432/vacaciones";
+    private final String USER = "postgres";
+    private final String PAS = "root";
+
+    public DriveManager() {
+
+        this.setDriverClassName("org.postgresql.Drive");
+        this.setUrl("jdbc:postgresql://localhost:5432/prexperimento");
+        this.setUsername("postgres");
+        this.setPassword("root");
+
     }
     /*
     public DriveManager(){
